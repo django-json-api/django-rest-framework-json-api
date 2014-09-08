d28f3a9b6049f22ac971a6aa87e47a121592457c
Greg Aker <greg@gregaker.net>
2014-09-08 13:21:00 -0500

    Failing sideload resources test.

--- example/api/urls.py
+++ example/api/urls.py
@@ -2,19 +2,25 @@
 Example app URLs
 """
 from django.conf.urls import patterns, include, url
-from .api import User, UserEmber, EmberUserModelViewSet, EmberDataMixinUserModelViewSet
+from .api import (
+    User, UserEmber, EmberUserModelViewSet, EmberDataMixinUserModelViewSet,
+    UserCarResource)
 
 from rest_framework import routers
 
 urlpatterns = patterns('',
     url(r'^user-default/(?P<pk>\d+)/$', User.as_view(), name='user-default'),
     url(r'^user-ember/(?P<pk>\d+)/$', UserEmber.as_view(), name='user-ember'),
-    url(r'^user-mixin-viewset/$', EmberDataMixinUserModelViewSet.as_view({'get': 'list'}),
+    url(r'^user-mixin-viewset/$',
+        EmberDataMixinUserModelViewSet.as_view({'get': 'list'}),
         name='mixin-user-list'),
     url(r'^user-viewset/$', EmberUserModelViewSet.as_view({'get': 'list'}),
         name='user-list'),
     url(r'^user-viewset/(?P<pk>\d+)/$',
         EmberUserModelViewSet.as_view(
-            {'get': 'retrieve', 'post': 'create', 'put': 'update'}), name='user-detail'),
+            {'get': 'retrieve', 'post': 'create', 'put': 'update'}),
+        name='user-detail'),
+    url(r'users-cars/$', UserCarResource.as_view(),
+        name='users-cars'),
 )
 
