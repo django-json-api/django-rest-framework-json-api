dba87f429a833f3583c78fd4002cbbefd8062088
Eric Honkanen <eric@epicowl.com>
2014-10-30 18:23:04 -0700

    Fixed structure issue

--- rest_framework_ember/renderers.py
+++ rest_framework_ember/renderers.py
@@ -1,10 +1,8 @@
 import copy
 import inflection
 
-from django.conf import settings
-
 from rest_framework import renderers
-from rest_framework_ember.utils import get_resource_name, get_key
+from rest_framework_ember.utils import get_resource_name
 
 
 class JSONRenderer(renderers.JSONRenderer):
@@ -20,7 +18,6 @@ class JSONRenderer(renderers.JSONRenderer):
     }
     """
     def render(self, data, accepted_media_type=None, renderer_context=None):
-
         view = renderer_context.get('view')
         resource_name = get_resource_name(view)
 
@@ -28,38 +25,29 @@ class JSONRenderer(renderers.JSONRenderer):
             return super(JSONRenderer, self).render(
                 data, accepted_media_type, renderer_context)
 
+        if isinstance(data, dict):
+            for key, value in data.items():
+                data[inflection.camelize(key, False)] = data.pop(key)
+
+        elif isinstance(data, (list, tuple)):
+            for obj in data:
+                if hasattr(obj, 'items'):
+                    for key, value in obj.items():
+                        obj[inflection.camelize(key, False)] = obj.pop(key)
+
+            if len(data) > 1:
+                resource_name = inflection.pluralize(resource_name)
+
         try:
             data_copy = copy.copy(data)
             content = data_copy.pop('results')
-
-            # Handle meta
-            for key, value in data_copy.items():
-                data_copy[get_key(key)] = data_copy.pop(key)
-
-            if isinstance(content, dict):
-                for key, value in content.items():
-                    content[get_key(key)] = content.pop(key)
-
-            elif isinstance(content, (list, tuple)):
-                for obj in content:
-                    if hasattr(obj, 'items'):
-                        for key, value in obj.items():
-                            obj[get_key(key)] = obj.pop(key)
-
-                if getattr(settings, 'REST_FRAMEWORK_PLURALIZE_KEYS', False):
-                    if len(content) > 1:
-                        resource_name = inflection.pluralize(resource_name)
-
             data = {resource_name : content, "meta" : data_copy}
         except (TypeError, KeyError, AttributeError) as e:
-
+            
             # Default behavior
             if not resource_name == 'data':
-                if isinstance(data, dict):
-                    for key, value in data.items():
-                        data[get_key(key)] = data.pop(key)
-                else:
-                    data = {resource_name: data}
+                for key, value in data.items():
+                    data[inflection.camelize(key, False)] = data.pop(key)
 
             data = {resource_name : data}
         return super(JSONRenderer, self).render(
