04738fa462b8aae6e9fd363ecf2dcbeeb6296776
Jonathan Senecal <jsenecal@zerofail.com>
2015-08-13 14:45:17 -0400

    Added format 'vnd.api+json' for the browsable API

--- rest_framework_json_api/renderers.py
+++ rest_framework_json_api/renderers.py
@@ -2,11 +2,10 @@
 Renderers
 """
 from collections import OrderedDict
+
 from rest_framework import renderers
 
 from . import utils
-from rest_framework.relations import RelatedField
-from rest_framework.settings import api_settings
 
 
 class JSONRenderer(renderers.JSONRenderer):
@@ -30,6 +29,7 @@ class JSONRenderer(renderers.JSONRenderer):
     """
 
     media_type = 'application/vnd.api+json'
+    format = 'vnd.api+json'
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
         # Get the resource name.
