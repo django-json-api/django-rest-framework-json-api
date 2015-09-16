3b84cc9d315c7c276596c991106f5ae06df6a260
Jerel Unruh <mail@unruhdesigns.com>
2015-09-15 20:13:38 -0500

    Switched example serializer to use ModelSerializer

--- example/serializers.py
+++ example/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+from example.models import Blog
 
 
-class BlogSerializer(serializers.Serializer):
+class BlogSerializer(serializers.ModelSerializer):
 
     class Meta:
+        model = Blog
         fields = ('name', )
