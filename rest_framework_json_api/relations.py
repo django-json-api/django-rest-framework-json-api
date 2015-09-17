2202e268582533f2d0d7a5337c7565f81f1db39a
Jonathan Senecal <jsenecal@zerofail.com>
2015-09-17 10:37:30 -0400

    Proper error messages

--- rest_framework_json_api/relations.py
+++ rest_framework_json_api/relations.py
@@ -1,5 +1,6 @@
 from rest_framework.exceptions import ValidationError
 from rest_framework.relations import *
+from django.utils.translation import ugettext_lazy as _
 
 
 class HyperlinkedRelatedField(HyperlinkedRelatedField):
@@ -7,6 +8,15 @@ class HyperlinkedRelatedField(HyperlinkedRelatedField):
     This field exists for the sole purpose of accepting PKs as well as URLs
     when data is submitted back to the serializer
     """
+    default_error_messages = {
+        'required': _('This field is required.'),
+        'no_match': _('Invalid hyperlink - No URL match.'),
+        'incorrect_match': _('Invalid hyperlink - Incorrect URL match.'),
+        'does_not_exist': _('Invalid hyperlink - Object does not exist.'),
+        'incorrect_type': _('Incorrect type. Expected URL string, received {data_type}.'),
+        'pk_does_not_exist': _('Invalid pk "{pk_value}" - object does not exist.'),
+        'incorrect_pk_type': _('Incorrect type. Expected pk value, received {data_type}.'),
+    }
 
     def __init__(self, **kwargs):
         self.pk_field = kwargs.pop('pk_field', None)
@@ -22,6 +32,6 @@ class HyperlinkedRelatedField(HyperlinkedRelatedField):
             try:
                 return self.get_queryset().get(pk=data)
             except ObjectDoesNotExist:
-                self.fail('does_not_exist', pk_value=data)
+                self.fail('pk_does_not_exist', pk_value=data)
             except (TypeError, ValueError):
-                self.fail('incorrect_type', data_type=type(data).__name__)
+                self.fail('incorrect_pk_type', data_type=type(data).__name__)
