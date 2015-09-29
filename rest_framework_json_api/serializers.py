d62b455f0095f7cb04d87965ef90e9a402919d68
Jonathan Senecal <xxinfinityxx@gmail.com>
2015-09-28 22:47:55 -0400

    Implemented IncludedResourcesValidationMixin

--- rest_framework_json_api/serializers.py
+++ rest_framework_json_api/serializers.py
@@ -1,4 +1,5 @@
 from django.utils.translation import ugettext_lazy as _
+from rest_framework.exceptions import ParseError
 from rest_framework.serializers import *
 
 from rest_framework_json_api.utils import format_relation_name, get_resource_type_from_instance, \
@@ -58,7 +59,29 @@ class SparseFieldsetsMixin(object):
         super(SparseFieldsetsMixin, self).__init__(*args, **kwargs)
 
 
-class HyperlinkedModelSerializer(SparseFieldsetsMixin, HyperlinkedModelSerializer):
+class IncludedResourcesValidationMixin(object):
+    def __init__(self, *args, **kwargs):
+        context = kwargs.get('context')
+        request = context.get('request') if context else None
+        view = context.get('view') if context else None
+
+        if request and view:
+            include_resources_param = request.query_params.get('include') if request else None
+            if include_resources_param:
+                included_resources = include_resources_param.split(',')
+                for included_field_name in included_resources:
+                    if not hasattr(view, 'included_serializers'):
+                        raise ParseError('This endpoint does not support the include parameter')
+                    if view.included_serializers.get(included_field_name) is None:
+                        raise ParseError(
+                            'This endpoint does not support the include parameter for field {}'.format(
+                                included_field_name
+                            )
+                        )
+        super(IncludedResourcesValidationMixin, self).__init__(*args, **kwargs)
+
+
+class HyperlinkedModelSerializer(IncludedResourcesValidationMixin, SparseFieldsetsMixin, HyperlinkedModelSerializer):
     """
     A type of `ModelSerializer` that uses hyperlinked relationships instead
     of primary key relationships. Specifically:
@@ -66,5 +89,30 @@ class HyperlinkedModelSerializer(SparseFieldsetsMixin, HyperlinkedModelSerialize
     * A 'url' field is included instead of the 'id' field.
     * Relationships to other instances are hyperlinks, instead of primary keys.
 
+    Included Mixins:
     * A mixin class to enable sparse fieldsets is included
+    * A mixin class to enable validation of included resources is included
+    """
+
+
+class ModelSerializer(IncludedResourcesValidationMixin, SparseFieldsetsMixin, ModelSerializer):
+    """
+    A `ModelSerializer` is just a regular `Serializer`, except that:
+
+    * A set of default fields are automatically populated.
+    * A set of default validators are automatically populated.
+    * Default `.create()` and `.update()` implementations are provided.
+
+    The process of automatically determining a set of serializer fields
+    based on the model fields is reasonably complex, but you almost certainly
+    don't need to dig into the implementation.
+
+    If the `ModelSerializer` class *doesn't* generate the set of fields that
+    you need you should either declare the extra/differing fields explicitly on
+    the serializer class, or simply use a `Serializer` class.
+
+
+    Included Mixins:
+    * A mixin class to enable sparse fieldsets is included
+    * A mixin class to enable validation of included resources is included
     """
