1f653056ad89564d69896af919492a9fb6c6d33e
Jonathan Senecal <jsenecal@zerofail.com>
2015-10-16 11:56:17 -0400

    Updated the tests to check for issue #131

--- example/tests/integration/test_includes.py
+++ example/tests/integration/test_includes.py
@@ -1,20 +1,25 @@
-import pytest, json
+import pytest
 from django.core.urlresolvers import reverse
+
 from example.tests.utils import load_json
 
 pytestmark = pytest.mark.django_db
 
 
 def test_included_data_on_list(multiple_entries, client):
-    multiple_entries[1].comments = []
     response = client.get(reverse("entry-list") + '?include=comments')
     included = load_json(response.content).get('included')
 
+    assert len(load_json(response.content)['data']) == len(multiple_entries)
     assert [x.get('type') for x in included] == ['comments']
+    assert (len([resource for resource in included if resource["type"] == "comments"]) ==
+            sum([entry.comment_set.count() for entry in multiple_entries]))
+
 
 def test_included_data_on_detail(single_entry, client):
     response = client.get(reverse("entry-detail", kwargs={'pk': single_entry.pk}) + '?include=comments')
     included = load_json(response.content).get('included')
 
     assert [x.get('type') for x in included] == ['comments']
-
+    assert (len([resource for resource in included if resource["type"] == "comments"]) ==
+            single_entry.comment_set.count())
