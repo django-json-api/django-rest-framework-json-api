146cff6fd9e928ee8c46f29270643548fda4d397
Jonathan Senecal <jsenecal@zerofail.com>
2015-11-27 10:45:11 -0500

    Refactored some of the utility functions to be part of the renderer class.

--- rest_framework_json_api/renderers.py
+++ rest_framework_json_api/renderers.py
@@ -1,9 +1,14 @@
 """
 Renderers
 """
+import copy
 from collections import OrderedDict
 
+from django.utils import six, encoding
+from rest_framework import relations
 from rest_framework import renderers
+from rest_framework.serializers import BaseSerializer, ListSerializer, ModelSerializer
+from rest_framework.settings import api_settings
 
 from . import utils
 
@@ -30,6 +35,301 @@ class JSONRenderer(renderers.JSONRenderer):
 
     media_type = 'application/vnd.api+json'
     format = 'vnd.api+json'
+
+    def extract_attributes(self, fields, resource):
+        data = OrderedDict()
+        for field_name, field in six.iteritems(fields):
+            # ID is always provided in the root of JSON API so remove it from attributes
+            if field_name == 'id':
+                continue
+            # Skip fields with relations
+            if isinstance(field, (relations.RelatedField, relations.ManyRelatedField, BaseSerializer)):
+                continue
+
+            # Skip read_only attribute fields when `resource` is an empty
+            # serializer. Prevents the "Raw Data" form of the browsable API
+            # from rendering `"foo": null` for read only fields
+            try:
+                resource[field_name]
+            except KeyError:
+                if fields[field_name].read_only:
+                    continue
+
+            data.update({
+                field_name: resource.get(field_name)
+            })
+
+        return utils.format_keys(data)
+
+    def extract_relationships(self, fields, resource, resource_instance):
+        # Avoid circular deps
+        from rest_framework_json_api.relations import ResourceRelatedField
+
+        data = OrderedDict()
+
+        # Don't try to extract relationships from a non-existent resource
+        if resource_instance is None:
+            return
+
+        for field_name, field in six.iteritems(fields):
+            # Skip URL field
+            if field_name == api_settings.URL_FIELD_NAME:
+                continue
+
+            # Skip fields without relations
+            if not isinstance(field, (relations.RelatedField, relations.ManyRelatedField, BaseSerializer)):
+                continue
+
+            source = field.source
+            try:
+                relation_instance_or_manager = getattr(resource_instance, source)
+            except AttributeError:
+                # if the field is not defined on the model then we check the serializer
+                # and if no value is there we skip over the field completely
+                serializer_method = getattr(field.parent, source, None)
+                if serializer_method and hasattr(serializer_method, '__call__'):
+                    relation_instance_or_manager = serializer_method(resource_instance)
+                else:
+                    continue
+
+            relation_type = utils.get_related_resource_type(field)
+
+            if isinstance(field, relations.HyperlinkedIdentityField):
+                # special case for HyperlinkedIdentityField
+                relation_data = list()
+
+                # Don't try to query an empty relation
+                relation_queryset = relation_instance_or_manager.all() \
+                    if relation_instance_or_manager is not None else list()
+
+                for related_object in relation_queryset:
+                    relation_data.append(
+                        OrderedDict([('type', relation_type), ('id', encoding.force_text(related_object.pk))])
+                    )
+
+                data.update({field_name: {
+                    'links': {
+                        "related": resource.get(field_name)},
+                    'data': relation_data,
+                    'meta': {
+                        'count': len(relation_data)
+                    }
+                }})
+                continue
+
+            if isinstance(field, ResourceRelatedField):
+                # special case for ResourceRelatedField
+                relation_data = {
+                    'data': resource.get(field_name)
+                }
+
+                field_links = field.get_links(resource_instance)
+                relation_data.update(
+                    {'links': field_links}
+                    if field_links else dict()
+                )
+                data.update({field_name: relation_data})
+                continue
+
+            if isinstance(field, (relations.PrimaryKeyRelatedField, relations.HyperlinkedRelatedField)):
+                relation_id = relation_instance_or_manager.pk if resource.get(field_name) else None
+
+                relation_data = {
+                    'data': (
+                        OrderedDict([('type', relation_type), ('id', encoding.force_text(relation_id))])
+                        if relation_id is not None else None)
+                }
+
+                relation_data.update(
+                    {'links': {'related': resource.get(field_name)}}
+                    if isinstance(field, relations.HyperlinkedRelatedField) and resource.get(field_name) else dict()
+                )
+                data.update({field_name: relation_data})
+                continue
+
+            if isinstance(field, relations.ManyRelatedField):
+
+                if isinstance(field.child_relation, ResourceRelatedField):
+                    # special case for ResourceRelatedField
+                    relation_data = {
+                        'data': resource.get(field_name)
+                    }
+
+                    field_links = field.child_relation.get_links(resource_instance)
+                    relation_data.update(
+                        {'links': field_links}
+                        if field_links else dict()
+                    )
+                    relation_data.update(
+                        {
+                            'meta': {
+                                'count': len(resource.get(field_name))
+                            }
+                        }
+                    )
+                    data.update({field_name: relation_data})
+                    continue
+
+                relation_data = list()
+                for related_object in relation_instance_or_manager.all():
+                    related_object_type = utils.get_instance_or_manager_resource_type(related_object)
+                    relation_data.append(OrderedDict([
+                        ('type', related_object_type),
+                        ('id', encoding.force_text(related_object.pk))
+                    ]))
+                data.update({
+                    field_name: {
+                        'data': relation_data,
+                        'meta': {
+                            'count': len(relation_data)
+                        }
+                    }
+                })
+                continue
+
+            if isinstance(field, ListSerializer):
+                relation_data = list()
+
+                serializer_data = resource.get(field_name)
+                resource_instance_queryset = list(relation_instance_or_manager.all())
+                if isinstance(serializer_data, list):
+                    for position in range(len(serializer_data)):
+                        nested_resource_instance = resource_instance_queryset[position]
+                        nested_resource_instance_type = utils.get_resource_type_from_instance(nested_resource_instance)
+                        relation_data.append(OrderedDict([
+                            ('type', nested_resource_instance_type),
+                            ('id', encoding.force_text(nested_resource_instance.pk))
+                        ]))
+
+                    data.update({field_name: {'data': relation_data}})
+                    continue
+
+            if isinstance(field, ModelSerializer):
+                relation_model = field.Meta.model
+                relation_type = utils.format_relation_name(relation_model.__name__)
+
+                data.update({
+                    field_name: {
+                        'data': (
+                            OrderedDict([
+                                ('type', relation_type),
+                                ('id', encoding.force_text(relation_instance_or_manager.pk))
+                            ]) if resource.get(field_name) else None)
+                    }
+                })
+                continue
+
+        return utils.format_keys(data)
+
+    def extract_included(self, fields, resource, resource_instance, included_resources):
+        # this function may be called with an empty record (example: Browsable Interface)
+        if not resource_instance:
+            return
+
+        included_data = list()
+        current_serializer = fields.serializer
+        context = current_serializer.context
+        included_serializers = utils.get_included_serializers(current_serializer)
+        included_resources = copy.copy(included_resources)
+
+        for field_name, field in six.iteritems(fields):
+            # Skip URL field
+            if field_name == api_settings.URL_FIELD_NAME:
+                continue
+
+            # Skip fields without relations or serialized data
+            if not isinstance(field, (relations.RelatedField, relations.ManyRelatedField, BaseSerializer)):
+                continue
+
+            try:
+                included_resources.remove(field_name)
+            except ValueError:
+                # Skip fields not in requested included resources
+                continue
+
+            try:
+                relation_instance_or_manager = getattr(resource_instance, field_name)
+            except AttributeError:
+                try:
+                    # For ManyRelatedFields if `related_name` is not set we need to access `foo_set` from `source`
+                    relation_instance_or_manager = getattr(resource_instance, field.child_relation.source)
+                except AttributeError:
+                    serializer_method = getattr(current_serializer, field.source)
+                    relation_instance_or_manager = serializer_method(resource_instance)
+
+            new_included_resources = [key.replace('%s.' % field_name, '', 1)
+                                      for key in included_resources
+                                      if field_name == key.split('.')[0]]
+            serializer_data = resource.get(field_name)
+
+            if isinstance(field, relations.ManyRelatedField):
+                serializer_class = included_serializers.get(field_name)
+                field = serializer_class(relation_instance_or_manager.all(), many=True, context=context)
+                serializer_data = field.data
+
+            if isinstance(field, relations.RelatedField):
+                serializer_class = included_serializers.get(field_name)
+                if relation_instance_or_manager is None:
+                    continue
+                field = serializer_class(relation_instance_or_manager, context=context)
+                serializer_data = field.data
+
+            if isinstance(field, ListSerializer):
+                serializer = field.child
+                model = serializer.Meta.model
+                relation_type = utils.format_relation_name(model.__name__)
+                relation_queryset = list(relation_instance_or_manager.all())
+
+                # Get the serializer fields
+                serializer_fields = utils.get_serializer_fields(serializer)
+                if serializer_data:
+                    for position in range(len(serializer_data)):
+                        serializer_resource = serializer_data[position]
+                        nested_resource_instance = relation_queryset[position]
+                        included_data.append(
+                            self.build_json_resource_obj(
+                                serializer_fields, serializer_resource, nested_resource_instance, relation_type
+                            )
+                        )
+                        included_data.extend(
+                            self.extract_included(
+                                serializer_fields, serializer_resource, nested_resource_instance, new_included_resources
+                            )
+                        )
+
+            if isinstance(field, ModelSerializer):
+                model = field.Meta.model
+                relation_type = utils.format_relation_name(model.__name__)
+
+                # Get the serializer fields
+                serializer_fields = utils.get_serializer_fields(field)
+                if serializer_data:
+                    included_data.append(
+                        self.build_json_resource_obj(serializer_fields, serializer_data, relation_instance_or_manager,
+                                                     relation_type)
+                    )
+                    included_data.extend(
+                        self.extract_included(
+                            serializer_fields, serializer_data, relation_instance_or_manager, new_included_resources
+                        )
+                    )
+
+        return utils.format_keys(included_data)
+
+    def build_json_resource_obj(self, fields, resource, resource_instance, resource_name):
+        resource_data = [
+            ('type', resource_name),
+            ('id', encoding.force_text(resource_instance.pk) if resource_instance else None),
+            ('attributes', self.extract_attributes(fields, resource)),
+        ]
+        relationships = self.extract_relationships(fields, resource, resource_instance)
+        if relationships:
+            resource_data.append(('relationships', relationships))
+        # Add 'self' link if field is present and valid
+        if api_settings.URL_FIELD_NAME in resource and \
+                isinstance(fields[api_settings.URL_FIELD_NAME], relations.RelatedField):
+            resource_data.append(('links', {'self': resource[api_settings.URL_FIELD_NAME]}))
+        return OrderedDict(resource_data)
 
     def render_relationship_view(self, data, accepted_media_type=None, renderer_context=None):
         # Special case for RelationshipView
@@ -106,8 +406,8 @@ class JSONRenderer(renderers.JSONRenderer):
                 resource = serializer_data[position]  # Get current resource
                 resource_instance = resource_serializer.instance[position]  # Get current instance
                 json_api_data.append(
-                    utils.build_json_resource_obj(fields, resource, resource_instance, resource_name))
-                included = utils.extract_included(fields, resource, resource_instance, included_resources)
+                    self.build_json_resource_obj(fields, resource, resource_instance, resource_name))
+                included = self.extract_included(fields, resource, resource_instance, included_resources)
                 if included:
                     json_api_included.extend(included)
         else:
@@ -115,8 +415,8 @@ class JSONRenderer(renderers.JSONRenderer):
             if hasattr(data, 'serializer'):
                 fields = utils.get_serializer_fields(data.serializer)
                 resource_instance = data.serializer.instance
-                json_api_data = utils.build_json_resource_obj(fields, data, resource_instance, resource_name)
-                included = utils.extract_included(fields, data, resource_instance, included_resources)
+                json_api_data = self.build_json_resource_obj(fields, data, resource_instance, resource_name)
+                included = self.extract_included(fields, data, resource_instance, included_resources)
                 if included:
                     json_api_included.extend(included)
             else:
