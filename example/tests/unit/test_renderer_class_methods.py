d154cd852bdb02743e9752179559a91b9f1a7f8c
Jonathan Senecal <jsenecal@zerofail.com>
2015-11-27 11:27:51 -0500

    Fix for Database access not allowed, use the "django_db" mark to enable it.

--- example/tests/unit/test_renderer_class_methods.py
+++ example/tests/unit/test_renderer_class_methods.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from rest_framework_json_api import serializers
 from rest_framework_json_api.renderers import JSONRenderer
 
+pytestmark = pytest.mark.django_db
 
 class ResourceSerializer(serializers.ModelSerializer):
     class Meta:
