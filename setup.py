348171e1f9edb04acf17be5cca95fe5bb4b844ab
Jerel Unruh <mail@unruhdesigns.com>
2016-04-29 13:16:34 -0500

    Update setup.py to classify as production/stable

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
         'inflection>=0.3.0'
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
