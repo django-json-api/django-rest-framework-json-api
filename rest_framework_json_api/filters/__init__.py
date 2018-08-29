db9e1f9b1b8fa7b961767fd15103b094bb4900e6
Alan Crosswell <alan@columbia.edu>
2018-08-29 16:38:26 -0400

    resolve @sliverc review method of using optional django-filter. See https://github.com/django-json-api/django-rest-framework-json-api/pull/466#discussion_r213599037

--- rest_framework_json_api/filters/__init__.py
+++ rest_framework_json_api/filters/__init__.py
@@ -1,2 +1,6 @@
+import pkgutil
 from .sort import JSONAPIOrderingFilter  # noqa: F401
-from .filter import JSONAPIDjangoFilter  # noqa: F401
+# If django-filter is not installed, no-op.
+if pkgutil.find_loader('django_filters') is not None:
+    from .filter import JSONAPIDjangoFilter  # noqa: F401
+del pkgutil
