2742d6087f6752e9aa3b5a793101c6e403151999
Alan Crosswell <alan@columbia.edu>
2018-08-29 13:46:08 -0400

    rename tests from filter to param

--- example/tests/test_filters.py
+++ example/tests/test_filters.py
@@ -307,30 +307,6 @@ class DJATestFilters(APITestCase):
                          ["invalid filter: filter[",
                           "invalid query parameter: filter["])
 
-    def test_filter_missing_right_bracket(self):
-        """
-        test for filter missing right bracket
-        """
-        response = self.client.get(self.url, data={'filter[headline': 'foobar'})
-        self.assertEqual(response.status_code, 400,
-                         msg=response.content.decode("utf-8"))
-        dja_response = response.json()
-        self.assertIn(dja_response['errors'][0]['detail'],
-                         ["invalid filter: filter[headline",
-                          "invalid query parameter: filter[headline"])
-
-    def test_filter_incorrect_brackets(self):
-        """
-        test for filter with incorrect brackets
-        """
-        response = self.client.get(self.url, data={'filter{headline}': 'foobar'})
-        self.assertEqual(response.status_code, 400,
-                         msg=response.content.decode("utf-8"))
-        dja_response = response.json()
-        self.assertIn(dja_response['errors'][0]['detail'],
-                         ["invalid filter: filter{headline}",
-                          "invalid query parameter: filter{headline}"])
-
     def test_filter_missing_rvalue(self):
         """
         test for filter with missing value to test against
@@ -355,3 +331,26 @@ class DJATestFilters(APITestCase):
         dja_response = response.json()
         self.assertEqual(dja_response['errors'][0]['detail'],
                          "missing filter[headline] test value")
+
+    def test_param_missing_right_bracket(self):
+        """
+        test for filter missing right bracket
+        """
+        response = self.client.get(self.url, data={'filter[headline': 'foobar'})
+        self.assertEqual(response.status_code, 400,
+                         msg=response.content.decode("utf-8"))
+        dja_response = response.json()
+        self.assertEqual(dja_response['errors'][0]['detail'],
+                         "invalid query parameter: filter[headline")
+
+    def test_param_incorrect_brackets(self):
+        """
+        test for filter with incorrect brackets
+        """
+        response = self.client.get(self.url, data={'filter{headline}': 'foobar'})
+        self.assertEqual(response.status_code, 400,
+                         msg=response.content.decode("utf-8"))
+        dja_response = response.json()
+        self.assertEqual(dja_response['errors'][0]['detail'],
+                         "invalid query parameter: filter{headline}")
+
