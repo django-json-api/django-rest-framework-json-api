b99bfe494271dd6ea5935f96c3a82b19de7e2a33
Oliver Sauder <sliverc@users.noreply.github.com>
2019-10-03 20:00:10 +0400

    Format field names correctly for metadata in OPTIONS requests (#713)
    Take into account settings JSON_API_FORMAT_FIELD_NAMES

--- rest_framework_json_api/metadata.py
+++ rest_framework_json_api/metadata.py
@@ -7,7 +7,7 @@ from rest_framework.metadata import SimpleMetadata
 from rest_framework.settings import api_settings
 from rest_framework.utils.field_mapping import ClassLookupDict
 
-from rest_framework_json_api.utils import get_related_resource_type
+from rest_framework_json_api.utils import format_value, get_related_resource_type
 
 
 class JSONAPIMetadata(SimpleMetadata):
@@ -83,7 +83,7 @@ class JSONAPIMetadata(SimpleMetadata):
         serializer.fields.pop(api_settings.URL_FIELD_NAME, None)
 
         return OrderedDict([
-            (field_name, self.get_field_info(field))
+            (format_value(field_name), self.get_field_info(field))
             for field_name, field in serializer.fields.items()
         ])
 
