5bd575de7b4967f000cf9d4692d7f6c3c5091ab7
Boris Pleshakov <bpleshakov@potehadevs.com>
2020-01-16 19:27:27 +0300

    Validate that body and url id match.
    A server MUST return 409 Conflict when processing a PATCH request in which the resource object’s type and id do not match the server’s endpoint.

--- rest_framework_json_api/parsers.py
+++ rest_framework_json_api/parsers.py
@@ -139,6 +139,14 @@ class JSONParser(parsers.JSONParser):
         if not data.get('id') and request.method in ('PATCH', 'PUT'):
             raise ParseError("The resource identifier object must contain an 'id' member")
 
+        if request.method in ('PATCH', 'PUT') and data.get('id') != view.kwargs[view.lookup_field]:
+            raise exceptions.Conflict(
+                "The resource object's id ({data_id}) does not match url ({url_id})}".format(
+                    data_id=data.get('id'),
+                    url_id=view.kwargs[view.lookup_field]
+                )
+            )
+
         # Construct the return data
         serializer_class = getattr(view, 'serializer_class', None)
         parsed_data = {'id': data.get('id')} if 'id' in data else {}
