9472e6147451cbdb695bed7133650cbe53409651
Boris Pleshakov <bpleshakov@potehadevs.com>
2020-04-10 17:40:51 +0300

    f whenever lookup_field and lookup_url_kwarg doesn't exist ignore.

--- rest_framework_json_api/parsers.py
+++ rest_framework_json_api/parsers.py
@@ -144,12 +144,7 @@ class JSONParser(parsers.JSONParser):
             raise ParseError("The resource identifier object must contain an 'id' member")
 
         if request.method in ('PATCH', 'PUT'):
-            # "or" syntax throws AttributeException which is silenced by DRF
-            lookup_url_kwarg = None
-            if hasattr(view, 'lookup_url_kwarg'):
-                lookup_url_kwarg = view.lookup_url_kwarg
-            elif hasattr(view, 'lookup_field'):
-                lookup_url_kwarg = view.lookup_field
+            lookup_url_kwarg = getattr(view, 'lookup_url_kwarg', None) or getattr(view, 'lookup_field', None)
             if lookup_url_kwarg and str(data.get('id')) != str(view.kwargs[lookup_url_kwarg]):
                 raise exceptions.Conflict(
                     "The resource object's id ({data_id}) does not match url's "
