f6810989d183a559d08fd3df945a947633bfe250
Boris Pleshakov <bpleshakov@potehadevs.com>
2020-04-14 15:26:15 +0300

    f unit test

--- example/tests/test_parsers.py
+++ example/tests/test_parsers.py
@@ -2,8 +2,11 @@ import json
 from io import BytesIO
 
 from django.test import TestCase, override_settings
+from rest_framework import views, status
 from rest_framework.exceptions import ParseError
+from rest_framework.response import Response
 
+from rest_framework_json_api import serializers
 from rest_framework_json_api.parsers import JSONParser
 
 
@@ -69,3 +72,51 @@ class TestJSONParser(TestCase):
 
         with self.assertRaises(ParseError):
             parser.parse(stream, None, self.parser_context)
+
+
+class DummySerializer(serializers.Serializer):
+    body = serializers.CharField()
+
+
+class DummyAPIView(views.APIView):
+    parser_classes = [JSONParser]
+    resource_name = 'dummy'
+
+    def patch(self, request, *args, **kwargs):
+        serializer = DummySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(status=status.HTTP_200_OK, data=serializer.validated_data)
+
+
+class TestParserOnAPIView(TestCase):
+
+    def setUp(self):
+        class MockRequest(object):
+            def __init__(self):
+                self.method = 'PATCH'
+
+        request = MockRequest()
+
+        self.parser_context = {'request': request, 'kwargs': {}, 'view': 'DummyAPIView'}
+
+        data = {
+            'data': {
+                'id': 123,
+                'type': 'strs',
+                'attributes': {
+                    'body': 'hello'
+                },
+            }
+        }
+
+        self.string = json.dumps(data)
+
+    def test_patch_doesnt_raise_attribute_error(self):
+        parser = JSONParser()
+
+        stream = BytesIO(self.string.encode('utf-8'))
+
+        data = parser.parse(stream, None, self.parser_context)
+
+        assert data['id'] == 123
+        assert data['body'] == 'hello'
