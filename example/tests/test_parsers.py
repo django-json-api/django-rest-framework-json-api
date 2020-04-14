c123aa56ef7248285866478d8320fbfe104f7200
Boris Pleshakov <bpleshakov@potehadevs.com>
2020-04-14 16:42:42 +0300

    + test with actual view usage

--- example/tests/test_parsers.py
+++ example/tests/test_parsers.py
@@ -1,13 +1,16 @@
 import json
 from io import BytesIO
 
+from django.conf.urls import url
 from django.test import TestCase, override_settings
+from django.urls import reverse
 from rest_framework import views, status
 from rest_framework.exceptions import ParseError
 from rest_framework.response import Response
 
 from rest_framework_json_api import serializers
 from rest_framework_json_api.parsers import JSONParser
+from rest_framework_json_api.renderers import JSONRenderer
 
 
 class TestJSONParser(TestCase):
@@ -74,18 +77,34 @@ class TestJSONParser(TestCase):
             parser.parse(stream, None, self.parser_context)
 
 
+class DummyDTO:
+    def __init__(self, response_dict):
+        for k, v in response_dict.items():
+            setattr(self, k, v)
+
+    @property
+    def pk(self):
+        return self.id if hasattr(self, 'id') else None
+
+
 class DummySerializer(serializers.Serializer):
     body = serializers.CharField()
+    id = serializers.IntegerField()
 
 
 class DummyAPIView(views.APIView):
     parser_classes = [JSONParser]
+    renderer_classes = [JSONRenderer]
     resource_name = 'dummy'
 
     def patch(self, request, *args, **kwargs):
-        serializer = DummySerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return Response(status=status.HTTP_200_OK, data=serializer.validated_data)
+        serializer = DummySerializer(DummyDTO(request.data))
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+
+urlpatterns = [
+    url(r'repeater$', DummyAPIView.as_view(), name='repeater'),
+]
 
 
 class TestParserOnAPIView(TestCase):
@@ -96,10 +115,10 @@ class TestParserOnAPIView(TestCase):
                 self.method = 'PATCH'
 
         request = MockRequest()
-
+        # To be honest view string isn't resolved into actual view
         self.parser_context = {'request': request, 'kwargs': {}, 'view': 'DummyAPIView'}
 
-        data = {
+        self.data = {
             'data': {
                 'id': 123,
                 'type': 'strs',
@@ -109,7 +128,7 @@ class TestParserOnAPIView(TestCase):
             }
         }
 
-        self.string = json.dumps(data)
+        self.string = json.dumps(self.data)
 
     def test_patch_doesnt_raise_attribute_error(self):
         parser = JSONParser()
@@ -120,3 +139,14 @@ class TestParserOnAPIView(TestCase):
 
         assert data['id'] == 123
         assert data['body'] == 'hello'
+
+    @override_settings(ROOT_URLCONF=__name__)
+    def test_patch_request(self):
+        url = reverse('repeater')
+        data = self.data
+        data['data']['type'] = 'dummy'
+        response = self.client.patch(url, data, content_type='application/vnd.api+json')
+        data = response.json()
+
+        assert data['data']['id'] == str(123)
+        assert data['data']['attributes']['body'] == 'hello'
