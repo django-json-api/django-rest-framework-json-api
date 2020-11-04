258e825d7ae95d7ee29bc0a7093fedbfc8daae44
Oliver Sauder <os@esite.ch>
2020-11-04 22:30:12 +0100

    Convert test_utils to pytest styled tests
    Added generic models as by example of DRF. Those are not example
    models as before but generic in representing different constellations
    possible with Django models.

---
+++ tests/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class DJAModel(models.Model):
+    """
+    Base for test models that sets app_label, so they play nicely.
+    """
+
+    class Meta:
+        app_label = 'tests'
+        abstract = True
+
+
+class BasicModel(DJAModel):
+    text = models.CharField(max_length=100)
+
+
+# Models for relations tests
+# ManyToMany
+class ManyToManyTarget(DJAModel):
+    name = models.CharField(max_length=100)
+
+
+class ManyToManySource(DJAModel):
+    name = models.CharField(max_length=100)
+    targets = models.ManyToManyField(ManyToManyTarget, related_name='sources')
+
+
+# ForeignKey
+class ForeignKeyTarget(DJAModel):
+    name = models.CharField(max_length=100)
+
+
+class ForeignKeySource(DJAModel):
+    name = models.CharField(max_length=100)
+    target = models.ForeignKey(ForeignKeyTarget, related_name='sources',
+                               on_delete=models.CASCADE)
