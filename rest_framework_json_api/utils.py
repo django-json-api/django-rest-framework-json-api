9d654feb5e1a5646c8ad3102a015228ce9eb411a
Oliver Sauder <os@esite.ch>
2020-12-11 21:34:10 +0400

    Minor changes to linting and changelog

--- rest_framework_json_api/utils.py
+++ rest_framework_json_api/utils.py
@@ -16,7 +16,6 @@ from django.utils.module_loading import import_string as import_class_from_dotte
 from django.utils.translation import gettext_lazy as _
 from rest_framework import exceptions
 from rest_framework.exceptions import APIException
-from rest_framework.serializers import ManyRelatedField
 
 from .settings import json_api_settings
 
