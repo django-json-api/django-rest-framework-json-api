081619296ac87fbd740bddf7b053e0a989df9a59
Oliver Sauder <sliverc@users.noreply.github.com>
2021-10-06 17:22:06 +0400

    Move pull request template to github directory (#991)
    This file is part of the github workflow and not the documentation
    so for consistency reason be moved to `.github` to be accompanied
    with the issue template.

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build", "pull_request_template.md"]
+exclude_patterns = ["_build"]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
